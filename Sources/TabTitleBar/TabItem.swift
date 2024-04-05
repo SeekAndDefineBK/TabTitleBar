bab55caad027428a509c9d0380c81c78961374cc
Brett Koster <skdfmanagement@icloud.com>
2024-04-04 20:30:13 -0500

    Created public initializer for TabItem

--- Sources/TabTitleBar/TabItem.swift
+++ Sources/TabTitleBar/TabItem.swift
@@ -10,7 +10,13 @@ import SwiftUI
 /// An Object that contains the Text view and the index it is represented by
 /// Conforms to Identifiable, you can define your own ID if necessary.
 public struct TabItem: Identifiable {
-    public let id: UUID = UUID()
+    public let id: UUID
     public let view: Text
     public let index: Int
+    
+    public init(id: UUID = UUID(), view: Text, index: Int) {
+        self.id = id
+        self.view = view
+        self.index = index
+    }
 }
