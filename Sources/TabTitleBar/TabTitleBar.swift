aae1eb83e8f27a635afbc5114056bdec5ce0f2ac
Brett Koster <skdfmanagement@icloud.com>
2024-06-21 15:58:21 -0500

    v1.1 Updates
    Added
    - changeTabLabel
    - changeTabSymbol
    - LargeExampleView
    - VeryLargeExampleView
    
    Removed
    - Buttons from SmallExampleView
    
    Enhancements
    - Added a ViewThatFits(in: .horizontal) to split between an HStack and a Menu based on how many views need to be rendered
    - Created ItemWithModifier to be reused whenever a TabItem.view needs to be rendered
    - Created ButtonLoopView so that buttons can be rendered with one component instead of several
    
    Simplified
    - SmallExampleView tabItem initialization

--- Sources/TabTitleBar/TabTitleBar.swift
+++ Sources/TabTitleBar/TabTitleBar.swift
@@ -3,41 +3,100 @@ import SwiftUI
 public struct TabTitleBar: View {
     @Binding var currentTabSelection: Int
     let tabItems: [TabItem]
-    
+    let changeTabLabel: String
+    let changeTabSymbol: String
     
     /// An HStack that animates between the currently selected item and the next selected item
     /// - Parameters:
     ///   - currentTabSelection: Binding<Int> that will determine which item is active / inactive
     ///   - tabItems: An Array of TabItems, an Object that
-    public init(currentTabSelection: Binding<Int>, tabItems: [TabItem]) {
+    ///   - changeTabLabel: a String value which will be populated next to the changeTabSymbol
+    ///   - changeTabSymbol: a String value which should correpond to an SF Symbol
+    public init(
+        currentTabSelection: Binding<Int>,
+        tabItems: [TabItem],
+        changeTabLabel: String = "Change Tab",
+        changeTabSymbol: String = "arrow.left.arrow.right.square"
+    ) {
         _currentTabSelection = currentTabSelection
         self.tabItems = tabItems
+        self.changeTabLabel = changeTabLabel
+        self.changeTabSymbol = changeTabSymbol
     }
     
-    public var body: some View {
-        HStack {
-            ForEach(tabItems) { item in
-                item.view
-                    .modifier(TabTitleModifier(currentTabSelection: $currentTabSelection, targetIndex: item.index))
+    // Shared View to use on multiple sizes
+    @ViewBuilder
+    /// Loops through all TabItems and creates a  styled button for each
+    /// - Parameter withSelectionIndicator: Bool with a default value of false. Will show a checkmark when item is currently selected
+    /// - Returns: Button Subviews created with each TabItem
+    public func ButtonLoopView(withSelectionIndicator: Bool = false) -> some View {
+        ForEach(tabItems) { item in
+            Button {
+                withAnimation {
+                    currentTabSelection = item.index
+                }
+            } label: {
+                // For use in a Menu view (or another view else like it)
+                if withSelectionIndicator {
+                    Label {
+                        ItemWithModifer(item: item)
+                    } icon: {
+                        let isSelected = item.index == currentTabSelection
+                        Image(systemName: isSelected ? "checkmark" : "")
+                            .accessibilityHidden(true)
+                            .accessibilityHint(isSelected ? "Active Tab" : "")
+                    }
+                } else {
+                    // Default view for smaller views
+                    ItemWithModifer(item: item)
+                }
             }
+            // Set the color to primary so that the default blue button isn't used
+            .foregroundStyle(.primary)
+        }
+    }
+    
+    @ViewBuilder
+    public func ItemWithModifer(item: TabItem) -> some View {
+        item.view
+            .modifier(TabTitleModifier(
+                currentTabSelection: $currentTabSelection,
+                index: item.index)
+            )
+    }
+            
+    public var body: some View {
+        ViewThatFits(in: .horizontal) {
+            // By default the view does not scroll
+            HStack {
+                ButtonLoopView()
+            }
+            
+            // When the too many tabs are present, the title will collapse into a menu
+            Menu {
+                ButtonLoopView(withSelectionIndicator: true)
+            } label: {
+                HStack {
+                    tabItems[currentTabSelection].view
+                        .modifier(SingleTabTitleModifier())
+                        .foregroundStyle(.foreground)
+                    
+                    Spacer()
+                    
+                    Label(changeTabLabel, systemImage: "arrow.left.arrow.right.square")
+                }
+            }
+            .environment(\.menuOrder, .fixed)
+            .frame(maxWidth: .infinity, alignment: .leading)
+            .accessibilityHint("Tap here to change the active view")
         }
     }
 }
 
-#Preview {
-    ExampleView()
-}
-
-struct ExampleView: View {
+struct SmallExampleView: View {
     @State var tabSelection: Int = 0
     
-    var tabItems: [TabItem] {
-        let item1 = TabItem(view: Text("Item 1"), index: 0)
-        let item2 = TabItem(view: Text("Item 2"), index: 1)
-        let item3 = TabItem(view: Text("Item 3"), index: 2)
-        
-        return [item1, item2, item3]
-    }
+    var tabItems: [TabItem] = Array(0...2).map({TabItem(view: Text("Item \($0)"), index: $0)})
     
     var body: some View {
         VStack {
@@ -45,23 +104,88 @@ struct ExampleView: View {
                 currentTabSelection: $tabSelection,
                 tabItems: tabItems
             )
-            
-            HStack {
-                Button("1") {
-                    tabSelection = 0
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Button("2") {
-                    tabSelection = 1
-                }
-                .buttonStyle(.borderedProminent)
-                
-                Button("3") {
-                    tabSelection = 2
-                }
-                .buttonStyle(.borderedProminent)
-            }
         }
     }
+}
+
+struct LargeExampleView: View {
+    @State var tabSelection: Int = 0
+    
+    var tabItems: [TabItem] = [
+        TabItem(
+            view: Text("Shoes"),
+            index: 0
+        ),
+        TabItem(
+            view: Text("Workouts"),
+            index: 1
+        ),
+        TabItem(
+            view: Text("Recents"),
+            index: 2
+        ),
+        TabItem(
+            view: Text("Equipment"),
+            index: 3
+        ),
+        TabItem(
+            view: Text("Routing"),
+            index: 4
+        ),
+        TabItem(
+            view: Text("Replacements"),
+            index: 5
+        ),
+        TabItem(
+            view: Text("Settings"),
+            index: 6
+        )
+    ]
+    
+    var body: some View {
+        TabTitleBar(
+            currentTabSelection: $tabSelection,
+            tabItems: tabItems
+        )
+    }
+}
+
+struct VeryLargeExampleView: View {
+    @State var tabSelection: Int = 0
+    
+    var tabItems: [TabItem] = [
+        TabItem(
+            view: Text("Long Text Here"),
+            index: 0
+        ),
+        TabItem(
+            view: Text("More Long Text Here"),
+            index: 1
+        ),
+        TabItem(
+            view: Text("This is supposed to break to a new line"),
+            index: 2
+        )
+    ]
+    
+    var body: some View {
+        TabTitleBar(
+            currentTabSelection: $tabSelection,
+            tabItems: tabItems,
+            changeTabLabel: "Switch Tab",
+            changeTabSymbol: "arrow.triangle.branch"
+        )
+    }
+}
+
+#Preview("Small") {
+    SmallExampleView()
+}
+
+#Preview("Large") {
+    LargeExampleView()
+}
+
+#Preview("VeryLarge") {
+    VeryLargeExampleView()
 }
